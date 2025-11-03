7894803d9fb0fe242850115c2e2f875861367036
saipriyapodili-byte <saipriya.podili@gmail.com>
2025-11-02 21:37:14 -0600

    Add files via upload

---
+++ TestData.java
@@ -0,0 +1,13 @@
+package week3.day1;
+
+public class TestData {
+
+	public void enterCredentials() {
+		System.out.println("This is enterCredentials methods");
+	}
+	public void navigateToHomePage() {
+		System.out.println("This is navigateToHomePage method");
+	}
+	
+
+}
