d8d3331a0bafd07e24f4394dc1cc31921db99450
saipriyapodili-byte <saipriya.podili@gmail.com>
2025-11-02 21:39:36 -0600

    Add files via upload

---
+++ ReverseTheOdd.java
@@ -0,0 +1,38 @@
+package week3.day1;
+
+public class ReverseTheOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String test="i am a software test";
+		
+		//step 1: Split the words and have it in an array
+		
+		String[] word=test.split(" ");
+		
+		//step2:Traverse through each word using loop
+		
+		for(int i=0; i>word.length;i++);{
+			
+			
+		
+		//step3:Find the odd index within the loop		
+		if( i%2 !=0) {
+			
+			//step4: Convert the String array into a character array
+			
+				
+				char[] chars=words[i].tocharArray();
+				
+				//step5:Reverse and print it
+				
+				
+				
+		
+		}
+	
+		}
+
+	}
+
+}
