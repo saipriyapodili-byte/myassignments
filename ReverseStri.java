a541a26ebf18bfdd2ef3f9436870e14e269ef61a
saipriyapodili-byte <saipriya.podili@gmail.com>
2025-11-02 21:38:35 -0600

    Add files via upload

---
+++ ReverseStri.java
@@ -0,0 +1,18 @@
+package week3.day1;
+
+public class ReverseStri {
+
+	public static void main(String[] args) {
+		
+		String companyName="null";
+		char[] chararray= companyName.toCharArray();
+		
+		for(int i=chararray.length -1; i>0; i--) {
+			
+			System.out.println(chararray[i]);
+		}
+		
+
+	}
+
+}
